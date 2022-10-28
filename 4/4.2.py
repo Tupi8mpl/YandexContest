032a3086981869f6c6025148f400b5f3e102c4f1
Ivan <i.tupikov12@gmail.com>
2022-10-29 02:59:46 +0500

    add 4.2

---
+++ 4/4.2.py
@@ -0,0 +1,221 @@
+import csv
+import re
+import prettytable
+from prettytable import PrettyTable
+
+
+class Vacancy:
+    number = 1
+
+    def __init__(self, dictionary: dict, headers_array: list):
+        self.headers = headers_array
+        self.field_dictionary = {}
+        if not dict:
+            self.empty = True
+        else:
+            self.set_vacancy(dictionary)
+
+    def set_vacancy(self, dictionary: dict):
+        self.field_dictionary = dictionary
+        self.empty = False
+        self.formatter()
+
+    def formatter(self):
+        self.delete_html_tags_and_replace_bool_fields()
+        self.salary_formatter()
+
+        date = self.field_dictionary["published_at"].split('T')[0].split('-')
+        self.field_dictionary['published_at'] = f"{date[2]}.{date[1]}.{date[0]}"
+
+        self.field_dictionary['key_skills'] = self.field_dictionary['key_skills'].replace('; ', '\n')
+        self.experience_formatter()
+
+    def delete_html_tags_and_replace_bool_fields(self):
+        for key in self.headers:
+            value = self.field_dictionary[key]
+            value = re.sub('<.*?>', '', str(value)).replace("\r\n", "; ").replace("\n", "; ")
+            value = ' '.join(value.split()).strip()
+            if value == "False":
+                value = "Нет"
+            elif value == "True":
+                value = "Да"
+            self.field_dictionary[key] = value
+
+    def salary_formatter(self):
+        salary_from = f'{int(float(self.field_dictionary["salary_from"])):,}'.replace(',', ' ')
+        salary_to = f'{int(float(self.field_dictionary["salary_to"])):,}'.replace(',', ' ')
+
+        self.field_dictionary['salary'] = f"{salary_from} - {salary_to} " \
+                                    f"({Data_Dictionary.dic_salary_currency[self.field_dictionary['salary_currency']]}) " \
+                                    f"{'(С вычетом налогов)' if self.field_dictionary['salary_gross'] != 'Да' else '(Без вычета налогов)'}"
+
+    def experience_formatter(self):
+        for field in self.field_dictionary.items():
+            key, value = field[0], field[1]
+            if value in Data_Dictionary.dic_experience:
+                self.field_dictionary[key] = Data_Dictionary.dic_experience[value]
+
+    def is_correct_by_filter(self, filter: str) -> bool:
+        if filter == "":
+            return True
+        else:
+            temp = filter.split(": ")
+            key, value = temp[0], temp[1]
+            if key == "Оклад":
+                return float(self.field_dictionary["salary_from"]) <= float(value) <= float(
+                    self.field_dictionary["salary_to"])
+            elif key == "Идентификатор валюты оклада":
+                return Data_Dictionary.dic_salary_currency[self.field_dictionary['salary_currency']] == value
+            elif key == "Навыки":
+                for skill in value.split(", "):
+                    if skill not in self.field_dictionary["key_skills"].split("\n"):
+                        return False
+                return True
+            else:
+                inv_dic_filter = {v: k for k, v in Data_Dictionary.dic_filter_for_table.items()}
+                return self.field_dictionary[inv_dic_filter[key]] == value
+
+    def get_values_for_table(self):
+        values = []
+        for field in Data_Dictionary.dic_naming:
+            if field == "№":
+                values.append(f'{Vacancy.number}')
+                Vacancy.number += 1
+            else:
+                values.append(self.field_dictionary[field])
+        return values
+
+    def is_empty(self) -> bool:
+        return self.empty
+
+
+class Data_Dictionary:
+    dic_naming = {"№": "№",
+                  "name": "Название",
+                  "description": "Описание",
+                  "key_skills": "Навыки",
+                  "experience_id": "Опыт работы",
+                  "premium": "Премиум-вакансия",
+                  "employer_name": "Компания",
+                  "salary": "Оклад",
+                  "area_name": "Название региона",
+                  "published_at": "Дата публикации вакансии"}
+
+    dic_filter_for_table = {"name": "Название",
+                            "description": "Описание",
+                            "key_skills": "Навыки",
+                            "experience_id": "Опыт работы",
+                            "premium": "Премиум-вакансия",
+                            "employer_name": "Компания",
+                            "salary": "Оклад",
+                            "salary_currency": "Идентификатор валюты оклада",
+                            "area_name": "Название региона",
+                            "published_at": "Дата публикации вакансии"}
+
+    dic_experience = {"noExperience": "Нет опыта",
+                      "between1And3": "От 1 года до 3 лет",
+                      "between3And6": "От 3 до 6 лет",
+                      "moreThan6": "Более 6 лет"}
+
+    dic_salary_currency = {"AZN": "Манаты",
+                           "BYR": "Белорусские рубли",
+                           "EUR": "Евро",
+                           "GEL": "Грузинский лари",
+                           "KZT": "Тенге",
+                           "RUR": "Рубли",
+                           "UAH": "Гривны",
+                           "USD": "Доллары",
+                           "UZS": "Узбекский сум"}
+
+
+def csv_reader(file_name: str, filter: str):
+    header_array = []
+    vacancies_array = []
+    is_empty_file = True
+    with open(file_name, 'r', newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        if not reader.fieldnames:
+            print("Пустой файл")
+            exit()
+        header_array = reader.fieldnames
+        for row in reader:
+            is_empty_file = False
+            if all(row.values()):
+                vacancy = Vacancy(row, header_array)
+                if vacancy.is_correct_by_filter(filter):
+                    vacancies_array.append(vacancy)
+    if is_empty_file:
+        print("Нет данных")
+        exit()
+    return vacancies_array, header_array
+
+
+def add_to_table(table: PrettyTable, data_vacancies: list):
+    count = 0
+    for vacancy in data_vacancies:
+        count += 1
+        if vacancy.is_empty():
+            continue
+        row = list(vacancy.get_values_for_table())
+        for i in range(len(row)):
+            if len(row[i]) > 100:
+                row[i] = row[i][0:100] + '...'
+        table.add_row(row)
+
+
+def print_table(table: PrettyTable, indexes: str, field_name: str):
+    start_index, end_index, fields_name = set_print_settings(indexes, field_name, len(table.rows))
+
+    print(table.get_string(start=start_index, end=end_index, fields=fields_name))
+
+
+def set_print_settings(indexes: str, field_name: str, table_size: int):
+    fields_name = ("№, " + field_name).split(', ')
+    end_index = table_size
+    if indexes == '':
+        start_index = 0
+    elif indexes.__contains__(' '):
+        indexes_array = indexes.split(' ')
+        start_index, end_index = int(indexes_array[0]) - 1, int(indexes_array[1]) - 1
+    else:
+        start_index = int(indexes) - 1
+
+    if field_name == '':
+        fields_name = ''
+    return start_index, end_index, fields_name
+
+
+def is_correct_filter(filter: str) -> bool:
+    if filter == "":
+        return True
+    if not filter.__contains__(": "):
+        print("Формат ввода некорректен")
+        return False
+    for field in Data_Dictionary.dic_filter_for_table.items():
+        if filter.__contains__(field[0]) or filter.__contains__(field[1]):
+            return True
+    print("Параметр поиска некорректен")
+    return False
+
+
+table = PrettyTable()
+table.hrules = prettytable.ALL
+table.field_names = Data_Dictionary.dic_naming.values()
+table.max_width = 20
+table.align = 'l'
+
+file_name = input()
+filter_for_table = input()
+indexes = input()
+field_name = input()
+
+if not is_correct_filter(filter_for_table):
+    exit()
+
+vacancies_array, headers_array = csv_reader(file_name, filter_for_table)
+
+if len(vacancies_array) == 0:
+    print("Ничего не найдено")
+else:
+    add_to_table(table, vacancies_array)
+    print_table(table, indexes, field_name)
